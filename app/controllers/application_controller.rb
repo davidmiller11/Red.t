0b8ec21c9aaf564106ded2ab85107d0c27223151
davidmiller11 <davidmiller11@gmail.com>
2014-04-08 18:00:59 -0400

    added rspec and controllers

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,15 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
+  helper_method :current_user
+
   def current_user
-    @_current_user ||= session[:current_user_id] && User.find(session[:current_user_id])
+    if session[:user_id]
+      return User.find(session[:user_id])
+    end
+  end
+
+  def require_authentication
+    redirect_to login_path unless current_user
   end
 end
--- app/controllers/logins_controller.rb
+++
@@ -1,7 +0,0 @@
-class LoginsController < ApplicationController
-
-  def create
-    if user = User.authenticate(params[:username], params[:password])
-  end
-
-end
